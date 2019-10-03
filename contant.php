bfafb3223f794f67ae731fbf6d0dbe9b35cf4009
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-10-03 10:33:27 +0700

    membuat file contant.php

---
+++ contant.php
@@ -0,0 +1,22 @@
+<?php 
+
+// define('NAMA', 'Erdika Rhamadan');
+
+// echo NAMA;
+// echo "<br>";
+
+// const UMUR = 17;
+
+// echo UMUR;
+
+class Coba {
+	const NAMA = 'Erdika';
+
+
+}
+
+echo Coba::NAMA;
+
+// __LINE__ untuk di baris berapa
+// __FILE__ untuk menampilkan direksi file
+//dll
